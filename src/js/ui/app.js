6a90c29de8cb697219fd6d4a3a97a9610208ec78
jwhyun <jwhyun@cafe24corp.com>
2019-09-24 16:07:26 +0900

    Update Theme.html

--- src/js/ui/app.js
+++ src/js/ui/app.js
@@ -11,54 +11,5 @@
     }
 
     var newAppSlider = new Cafe24.SwiperSlider('#newApp', options).init();
-    var bestThemeSlider = new Cafe24.SwiperSlider('#bestTheme', options).init();
-    var newThemeSlider = new Cafe24.SwiperSlider('#newTheme', options).init();
-    var mobileTheme = new Cafe24.SwiperSlider('#mobileTheme', options).init();
-
-
-    var combineMobileOptions = {
-        loop: true,
-        speed: 1200,
-        autoplay: false,
-        // autoplay: {
-        //     delay: 4000,
-        //     disableOnInteraction: false,
-        // },
-        slidesPerView: 'auto',
-        navigation: false,
-        pagination: false,
-        watchSlidesVisibility: true,
-        watchSlidesProgress: true,
-    };
-    var combineMobileSlider = new Cafe24.SwiperSlider('#combineMobile', combineMobileOptions).init();
-    var combinePcOptions = {
-        loop: true,
-        speed: 1200,
-        autoplay: false,
-        // autoplay: {
-        //     delay: 4000,
-        //     disableOnInteraction: false,
-        // },
-        slidesPerView: 'auto',
-        slideToClickedSlide: true,
-        pagination: false,
-        thumbs: {
-            swiper: combineMobileSlider,
-        }
-    };
-    var combinePcSlider = new Cafe24.SwiperSlider('#combinePC', combinePcOptions).init();
-
-    var options2 = {
-        loop: true,
-        loopedSlides: 4,
-        loopAdditionalSlides: 4,
-        slidesPerGroup: 4,
-        speed: 1200,
-        autoplay: false,
-        slidesPerView: 'auto',
-        // pagination: false,
-    }
-
-
 
 })(Cafe24.UI);
