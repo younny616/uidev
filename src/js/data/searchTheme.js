9b34f9ce346e7ae76622ebb8abd947cf657b9c37
jwhyun <jwhyun@cafe24corp.com>
2019-10-07 18:11:20 +0900

    search filter

---
+++ src/js/data/searchTheme.js
@@ -0,0 +1,76 @@
+const searchTheme = [
+    {
+        src: '/images/newTheme1.jpg',
+        title: 'Theme Title',
+        subTitle: '반응형',
+        price: '50,000원',
+    },
+    {
+        src: '/images/newTheme2.jpg',
+        title: 'Theme Title',
+        subTitle: 'PC',
+        price: '150,000원',
+    },
+    {
+        src: '/images/newTheme3.jpg',
+        title: 'Theme Title',
+        subTitle: 'PC+Mobile',
+        price: '50,000원',
+    },
+    {
+        src: '/images/newTheme4.jpg',
+        title: 'Theme Title',
+        subTitle: 'Mobile',
+        price: '150,000원',
+    },
+    {
+        src: '/images/minimalTheme1.jpg',
+        title: 'Theme Title',
+        subTitle: '반응형',
+        price: '50,000원',
+    },
+    {
+        src: '/images/minimalTheme2.jpg',
+        title: 'Theme Title',
+        subTitle: 'PC',
+        price: '150,000원',
+    },
+    {
+        src: '/images/minimalTheme3.jpg',
+        title: 'Theme Title',
+        subTitle: 'PC+Mobile',
+        price: '50,000원',
+    },
+    {
+        src: '/images/minimalTheme4.jpg',
+        title: 'Theme Title',
+        subTitle: 'Mobile',
+        price: '150,000원',
+    },
+    {
+        src: '/images/freeTheme1.jpg',
+        title: 'Theme Title',
+        subTitle: '반응형',
+        price: '50,000원',
+    },
+    {
+        src: '/images/freeTheme2.jpg',
+        title: 'Theme Title',
+        subTitle: 'PC',
+        price: '150,000원',
+    },
+    {
+        src: '/images/freeTheme3.jpg',
+        title: 'Theme Title',
+        subTitle: 'PC+Mobile',
+        price: '50,000원',
+    },
+    {
+        src: '/images/freeTheme4.jpg',
+        title: 'Theme Title',
+        subTitle: 'Mobile',
+        price: '150,000원',
+    },
+]
+
+module.exports = searchTheme;
