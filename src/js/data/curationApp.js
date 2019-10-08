4033edbba514863f8cddc068fd21a7883e356703
younny616 <younny616@gmail.com>
2019-10-08 14:40:54 +0900

    'ny'

---
+++ src/js/data/curationApp.js
@@ -0,0 +1,60 @@
+const curationApp = [
+    {
+        src: '/images/newApp1.jpg',
+        title: '인스타그램 위젯',
+        subTitle: '마케팅',
+        price: '50,000원',
+        description: '인스타그램 계정을 쇼핑몰에 연결하고 고객과의 관계를 강화 하세요',
+    },
+    {
+        src: '/images/newApp2.jpg',
+        title: 'KeepGrow 고객관리유지툴',
+        subTitle: '배너',
+        price: '55,000원',
+        description: '입점없이 최대 20여개의 대형쇼핑몰 동시 판매 무제한 주문 수집 가능 지금 바로 사용하세요. 내용이 길어지면 최대 3줄까지 노출',
+    },
+    {
+        src: '/images/newApp3.jpg',
+        title: '배너관리',
+        subTitle: '상품 디스플레이',
+        price: '100,000원',
+        description: '원하는 시점에 원하는 이미지를 선택하여 배너 이미지 간편하게 교체합니다.',
+    },
+    {
+        src: '/images/newApp4.jpg',
+        title: '신규출시된 앱 배너 제목 입력영역 최대 두줄까지 노출되며 말줄임 처리',
+        subTitle: '카테고리',
+        price: '70,000원',
+        description: '긴상세페이지에서 스크롤 시 옵션을 노출 상단, 색상 꾸미기가 가능합니다. 최대 3줄까지 노출되며 80자 내외로 보여지게 됩니다. 이후 말줄임 처리 됩니다.',
+    },
+    {
+        src: '/images/newApp1.jpg',
+        title: '인스타그램 위젯',
+        subTitle: '마케팅',
+        price: '50,000원',
+        description: '인스타그램 계정을 쇼핑몰에 연결하고 고객과의 관계를 강화 하세요',
+    },
+    {
+        src: '/images/newApp2.jpg',
+        title: 'KeepGrow 고객관리유지툴',
+        subTitle: '배너',
+        price: '55,000원',
+        description: '입점없이 최대 20여개의 대형쇼핑몰 동시 판매 무제한 주문 수집 가능 지금 바로 사용하세요. 내용이 길어지면 최대 3줄까지 노출',
+    },
+    {
+        src: '/images/newApp3.jpg',
+        title: '배너관리',
+        subTitle: '상품 디스플레이',
+        price: '100,000원',
+        description: '원하는 시점에 원하는 이미지를 선택하여 배너 이미지 간편하게 교체합니다.',
+    },
+    {
+        src: '/images/newApp4.jpg',
+        title: '신규출시된 앱 배너 제목 입력영역 최대 두줄까지 노출되며 말줄임 처리',
+        subTitle: '카테고리',
+        price: '70,000원',
+        description: '긴상세페이지에서 스크롤 시 옵션을 노출 상단, 색상 꾸미기가 가능합니다. 최대 3줄까지 노출되며 80자 내외로 보여지게 됩니다. 이후 말줄임 처리 됩니다.',
+    },
+]
+
+module.exports = curationApp;
