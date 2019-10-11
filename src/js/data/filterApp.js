05d0e48211fd97a57c9291d10fd24bcf2da1e41d
jwhyun <jwhyun@cafe24corp.com>
2019-10-11 17:10:31 +0900

    global button

--- src/js/data/filterApp.js
+++ src/js/data/filterApp.js
@@ -1,13 +1,16 @@
 const filterApp = [
     {
+        type: 'category',
         title: '카테고리',
         value : ['배너', '팝업/알림', '상품상세 편집', '상품 디스플레이', '마케팅', '고객지원', '판매/전환', '분석/통계', '기타'],
     },
     {
+        type: 'price',
         title: '가격',
         value : ['무료', '유료'],
     },
     {
+        type: 'partners',
         title: '파트너등급',
         value : ['플래티넘', '골드', '실버', '브론즈'],
     },
