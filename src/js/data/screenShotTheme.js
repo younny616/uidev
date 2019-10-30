cfcf6106beaeec7b9f97f180d9cc3c048e2805b3
younny616 <younny616@gmail.com>
2019-10-30 18:52:33 +0900

    'ny'

--- src/js/data/screenShotTheme.js
+++ src/js/data/screenShotTheme.js
@@ -1,12 +1,12 @@
 const screenShot = [
   {
-    src: 'https://appservice-img.s3.amazonaws.com/apps/duf3QzSBO67dfGaWKwhfGB/KR/screenshot1/thumbnail/image?1557305474'
+    src: '/images/@img_theme_screenshot.jpg'
+  },
+  {
+    src: '/images/@img_app_screenshot.jpg'
   },
   {
     src: 'https://appservice-img.s3.amazonaws.com/apps/duf3QzSBO67dfGaWKwhfGB/KR/screenshot2/thumbnail/image?1557305474'
-  },
-  {
-    src: 'https://orangemsg.com/cafe24/app/images/cafe24_detail.png'
   },
   {
     src: 'https://orangemsg.com/cafe24/app/images/cafe24_detail_add.png'
