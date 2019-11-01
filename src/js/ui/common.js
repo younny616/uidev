4dae8a066410deb957168ed17c2ba37b54877eeb
younny616 <younny616@gmail.com>
2019-11-01 17:36:04 +0900

    story

--- src/js/ui/common.js
+++ src/js/ui/common.js
@@ -64,6 +64,38 @@ function goTop(){
     });
 } goTop();
 
+function mTab() {
+    $('.mTab').each(function(){
+        var selected = $(this).find('> ul > li.selected > a');
+        if(selected.siblings('ul').length <= 0){
+            $(this).removeClass('gExtend');
+        }
+    });
+
+    $('body').delegate('.eTab a', 'click', function(e){
+        var _li = $(this).parent('li').addClass('selected').siblings().removeClass('selected'),
+            _target = $(this).attr('href'),
+            _siblings = $(_target).attr('class'),
+            _arr = _siblings.split(" "),
+            _classSiblings = '.'+_arr[0];
+
+        $(_target).show().siblings(_classSiblings).hide();
+
+        // gExtend ctrl
+        var mtab = $(this).parents('.mTab:first');
+        if($(this).siblings('ul').length > 0){
+            if(!mtab.hasClass('gExtend')){
+                mtab.addClass('gExtend');
+            }
+        } else {
+            if($(this).parents('ul:first').siblings('a').length <= 0){
+                mtab.removeClass('gExtend');
+            }
+        }
+        e.preventDefault();
+    });
+} mTab();
+
 // toolTip
 // function mTooltip(){
 //     // 고정
